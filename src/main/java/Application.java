46654c0f2af93a344b3b496cda51d13005e4125c
Georgiy Levchenko <fan-mag@mail.ru>
2019-11-28 13:05:34 +0300

    Types are changed to Generics: KP VP are key/value for producer KB, VB are key/value for buffer KC, VC are key/value for consumer

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -16,38 +16,38 @@ import java.util.List;
 import java.util.Properties;
 import java.util.concurrent.ExecutionException;
 
-public class Application {
-    static class Buffer {
-        private static final List<Pair<String, String>> messages = new ArrayList<>();
+public class Application<KC, VC, KP, VP, KB, VB> {
+    class Buffer {
+        private final List<Pair<KB, VB>> messages = new ArrayList<>();
 
-        synchronized static List<Pair<String, String>> messages() {
+        synchronized List<Pair<KB, VB>> messages() {
             return messages;
         }
 
-        synchronized static void putIntoBuffer(String key, String value) {
-            /* Input queue actions */
-
-
-            /*                     */
-            messages.add(new Pair<>(key, value));
+        synchronized void putIntoBuffer(KC key, VC value) {
+            /* INPUT MESSAGE ACTIONS START */
+            KB keyBuffer = (KB) key;
+            VB valueBuffer = (VB) value;
+            /* INPUT MESSAGE ACTIONS END */
+            messages.add(new Pair<>(keyBuffer, valueBuffer));
         }
 
-        synchronized static Pair<String, String> getFromBuffer() {
-            String key = messages.get(0).getKey();
-            String value = messages.get(0).getValue();
+        synchronized Pair<KP, VP> getFromBuffer() {
+            KB key = messages.get(0).getKey();
+            VB value = messages.get(0).getValue();
             messages.remove(0);
-            /* Output message actions */
-
-
-            /*                        */
-            return new Pair<>(key, value);
+            /* OUTPUT MESSAGE ACTIONS START */
+            KP keySend = (KP) key;
+            VP valueSend = (VP) value;
+            /* OUTPUT MESSAGE ACTIONS END */
+            return new Pair<>(keySend, valueSend);
         }
     }
 
-    static class ConsumerThread implements Runnable {
-        private Consumer<String, String> consumer;
+    class ConsumerThread implements Runnable {
+        private Consumer<KC, VC> consumer;
 
-        ConsumerThread(Consumer<String, String> consumer) {
+        ConsumerThread(Consumer<KC, VC> consumer) {
             this.consumer = consumer;
         }
 
@@ -56,17 +56,18 @@ public class Application {
             final int giveUp = Integer.parseInt((String) getInputProperty("poll_retries"));
             int noRecordsCount = 0;
             while (true) {
-                final ConsumerRecords<String, String> consumerRecords =
+                final ConsumerRecords<KC, VC> consumerRecords =
                         consumer.poll(Duration.ofMillis(Long.parseLong(getInputProperty("poll_interval").toString())));
                 if (consumerRecords.count() == 0 && noRecordsCount++ > giveUp)
                     break;
                 else {
+                    if (!consumerRecords.isEmpty()) noRecordsCount = 0;
                     consumerRecords.forEach(record ->
                     {
                         System.out.printf("Consumer Record:(Key:%s, Value:%s, Partition:%d, Offset:%d)%n",
                                 record.key(), record.value(),
                                 record.partition(), record.offset());
-                        Buffer.putIntoBuffer(record.key(), record.value());
+                        buffer.putIntoBuffer(record.key(), record.value());
                     });
                     consumer.commitAsync();
                 }
@@ -77,11 +78,11 @@ public class Application {
         }
     }
 
-    static class ProducerThread implements Runnable {
-        private Producer<String, String> producer;
+    class ProducerThread implements Runnable {
+        private Producer<KP, VP> producer;
         private String topic;
 
-        ProducerThread(Producer<String, String> producer, String topic) {
+        ProducerThread(Producer<KP, VP> producer, String topic) {
             this.producer = producer;
             this.topic = topic;
         }
@@ -91,16 +92,16 @@ public class Application {
             while (!stop) {
                 try {
                     Thread.sleep(1);
-                    if (!Buffer.messages().isEmpty()) {
-                        Pair<String, String> pair = Buffer.getFromBuffer();
-                        String key = pair.getKey();
-                        String value = pair.getValue();
-                        final ProducerRecord<String, String> record = new ProducerRecord<>(topic, key, value);
+                    if (!buffer.messages().isEmpty()) {
+                        Pair<KP, VP> pair = buffer.getFromBuffer();
+                        KP key = pair.getKey();
+                        VP value = pair.getValue();
+                        final ProducerRecord<KP, VP> record = new ProducerRecord<>(topic, key, value);
                         RecordMetadata metadata = producer.send(record).get();
                         System.out.printf("Producer record:(Key:%s, Value:%s, Partition:%d, Offset:%d) %n",
                                 key, value, metadata.partition(), metadata.offset());
                         Thread.sleep(Long.parseLong(getOutputProperty("pull_interval").toString()) - 1);
-                        System.out.printf("Messages in the queue: %d%n", Buffer.messages().size());
+                        System.out.printf("Messages in the queue: %d%n", buffer.messages().size());
                     }
                 } catch (InterruptedException | ExecutionException e) {
                     e.printStackTrace();
@@ -113,22 +114,23 @@ public class Application {
         }
     }
 
-    private static final Properties kafkaInputProps = new Properties();
-    private static final Properties kafkaOutputProps = new Properties();
-    private static Consumer<String, String> consumer;
-    private static Producer<String, String> producer;
-    private static String producerTopic;
-    private static volatile boolean stop = false;
+    private final Properties kafkaInputProps = new Properties();
+    private final Properties kafkaOutputProps = new Properties();
+    private Consumer<KC, VC> consumer;
+    private Producer<KP, VP> producer;
+    private String producerTopic;
+    private volatile boolean stop = false;
+    private Buffer buffer = new Buffer();
 
-    private static Object getInputProperty(String key) {
+    private Object getInputProperty(String key) {
         return kafkaInputProps.get(key);
     }
 
-    private static Object getOutputProperty(String key) {
+    private Object getOutputProperty(String key) {
         return kafkaOutputProps.get(key);
     }
 
-    private static void init() throws IOException {
+    private void init() throws IOException {
         kafkaInputProps.load(new FileReader("src/main/resources/kafka-input.properties"));
         kafkaOutputProps.load(new FileReader("src/main/resources/kafka-output.properties"));
         consumer = new KafkaConsumer<>(kafkaInputProps);
@@ -137,21 +139,22 @@ public class Application {
         producerTopic = (String) getOutputProperty("topic");
     }
 
-    private static void startConsumer() {
+    private void startConsumer() {
         ConsumerThread consumerThread = new ConsumerThread(consumer);
         Thread thread = new Thread(consumerThread);
         thread.start();
     }
 
-    private static void startProducer() {
+    private void startProducer() {
         ProducerThread producerThread = new ProducerThread(producer, producerTopic);
         Thread thread = new Thread(producerThread);
         thread.start();
     }
 
     public static void main(String[] args) throws IOException {
-        init();
-        startConsumer();
-        startProducer();
+        Application<String, String, String, Integer, String, String> app = new Application<>();
+        app.init();
+        app.startConsumer();
+        app.startProducer();
     }
 }
